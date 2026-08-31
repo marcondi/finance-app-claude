107976e1f221176edccb5e6d37dfcf622966dc62
Marcondes <72446393+marcondi@users.noreply.github.com>
2026-08-31 09:00:37 -0400

    Update Supabase anon key in supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,6 +6,6 @@ const supabaseUrl =
 
 const supabaseAnonKey = 
   process.env.REACT_APP_SUPABASE_ANON_KEY || 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vb2VnYmJ2cndpZmlsYXZsdmd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNTk5NTAsImV4cCI6MjA4NTgzNTk1MH0.x6wDd7c8V3eb1gYgQcEILEBEJKkPfJuF4o2_UuAV7Gk';
+  'sb_publishable_8iCmRDDPfAXCrZteb1rG-w_4DGlbmhK';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
